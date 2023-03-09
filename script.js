f4c81f24a8fd53fcf76cd233932f4c4517cda584
oibasqada <tumarbekovmax@gmail.com>
2023-03-09 17:05:28 +0600

    if else hw js3

--- script.js
+++ script.js
@@ -1,11 +1,125 @@
-function greeting(user, day){
-        if (day >= 32){
-                return `dear ${user}! There can't be in month more days than 31!`
-        } else if (day % 2 === 0){
-                return `welcome, ${user} how are you? today is ${day}, great day`
-        } else {
-                return `welcome, ${user} how are you? today is ${day}, simple day`
-        }
-}
 
-console.log(greeting( 'Nurali', 41))
+
+// function greeting(user){
+//         if (user === 'admin'){
+//                 return `dear ${user}, welcome!`
+//         } else {
+//                 return `false not correct user`
+//         }
+// }
+//
+// console.log(greeting( 'admin'))
+
+//-----------------------------------------------------------------
+
+// function isFebruary(day){
+//         if (day <= 28){
+//                 return `today is ${day} of february`
+//         } else if (day<=31) {
+//                 return `on february only 28 day, your day is ${day} of another month`
+//         } else {
+//                 return "There can't be in month more days than 31!"
+//         }
+// }
+//
+// console.log(isFebruary( 20))
+
+//-----------------------------------------------------------------
+
+// function equalityCheck(num){
+//         if (num !== 50){
+//                 return `${num} is not correct`
+//         } else {
+//                 return "Correct"
+//         }
+// }
+//
+// console.log(equalityCheck( 50))
+
+//-----------------------------------------------------------------
+
+// function ageCheck(age, user){
+//         if (age >= 18){
+//                 return `${user} is ${age} old. Welcome`
+//         } else {
+//                 return `${user} is too young, code off!`
+//         }
+// }
+//
+// console.log(ageCheck( 18, 'ehehe' ))
+
+//-----------------------------------------------------------------
+
+// function userName(user){
+//         if (user.length <= 2){
+//                 return `${user} name is too short (min 3)`
+//         } else {
+//                 return `${user} name accepted`
+//         }
+// }
+//
+// console.log(userName( 'max'))
+
+//-----------------------------------------------------------------
+
+// function userName(user){
+//         if (user.length >= 6){
+//                 return `${user} name is too long (max 5)`
+//         } else {
+//                 return `${user} name accepted`
+//         }
+// }
+//
+// console.log(userName( 'maxs'))
+
+//-----------------------------------------------------------------
+
+// function userAge(age){
+//         if (typeof (age) === 'number') {
+//                 return `${age} age accepted`
+//         } else {
+//                 return `${age} must be number`
+//         }
+// }
+//
+// console.log(userAge( 20))
+
+//-----------------------------------------------------------------
+
+// function userAge(age){
+//         if (typeof (age) === 'number'){
+//                 return `${age} must be a string`
+//         } else if ( age < 23){
+//                 return `must be older than 23`
+//         } else {
+//                 return 'accepted'
+//         }
+// }
+//
+// console.log(userAge( '23'))
+
+//-----------------------------------------------------------------
+
+// function weekDay(day){
+//         if ( day === 1){
+//                 return `${day} is Monday`
+//         } else if ( day === 2){
+//                 return `${day} is Tuesday`
+//         } else if ( day === 3){
+//                 return `${day} is Wednesday`
+//         } else if ( day === 4) {
+//                 return `${day} is Thursday`
+//         } else if ( day === 5) {
+//                 return `${day} is Friday`
+//         } else if ( day === 6){
+//                 return `${day} is weekend`
+//         } else if ( day === 7) {
+//                 return `${day} is weekend`
+//         } else {
+//                 return 'There is only 7 day in week'
+//         }
+// }
+//
+// console.log(weekDay( 8))
+
+
